604848d96ba42c6e78863b7775d2d80a1166d386
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-03-18 21:46:43 +0600

    Complete Project

---
+++ db.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2022 at 04:43 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud-app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_data`
+--
+
+CREATE TABLE `student_data` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `rool` int(255) NOT NULL,
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student_data`
+--
+
+INSERT INTO `student_data` (`id`, `name`, `rool`, `img`) VALUES
+(17, 'Faras Uddin Rubel', 3, 'IMG_0022.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student_data`
+--
+ALTER TABLE `student_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student_data`
+--
+ALTER TABLE `student_data`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
